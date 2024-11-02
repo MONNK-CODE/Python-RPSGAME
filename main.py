1adc070e18a87e0a780a1e01949da4f5bcfd929d
Muhais Olatundun <monnkcode@gmail.com>
2024-11-02 03:59:01 -0500

    updated random

---
+++ main.py
@@ -0,0 +1,27 @@
+import random as r
+materials = ["rock", "paper", "scissors"]
+restart = 'yes'
+
+while restart.lower() == 'yes':
+    computer_choice = r.choice(materials)
+    #print("Computer chose: ", computer_choice)
+    user_choice = input("Choose Rock, Paper, or Scissors: ").strip().lower()
+
+    if user_choice not in materials:
+        print("Invalid choice. Please choose Rock, Paper, or Scissors.")
+        continue
+    print("You chose: ", user_choice)
+    print("Computer chose: ", computer_choice)
+
+    if user_choice == computer_choice:
+        print("It's a tie!")
+
+    elif (user_choice == "rock" and computer_choice == "scissors") or \
+        (user_choice == "paper" and computer_choice == "rock") or \
+        (user_choice == "scissors" and computer_choice == "paper"):
+        print("You win!")
+
+    else:
+        print("Computers wins!")
+
+    restart = input('Do you want to play again?(yes or no) ').strip().lower()
